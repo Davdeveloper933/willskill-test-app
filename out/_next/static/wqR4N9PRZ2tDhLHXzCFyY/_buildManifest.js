354bd2df6cf5ebf07565fa5883e56597909d6d1e
David Istomin <daveistomin@gmail.com>
2022-11-04 23:27:57 +0400

    Initial out subtree commit gh-pages

---
+++ out/_next/static/wqR4N9PRZ2tDhLHXzCFyY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5c1035fb81492bb8.css","static/chunks/pages/index-dd67dcbc4fd7df18.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
