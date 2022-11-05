25152b8123536eefa3b21b1c14b6e9c2331d586e
David Istomin <daveistomin@gmail.com>
2022-11-05 17:53:19 +0400

    deploy

--- out/_next/static/qd2ToxFwevUB_attKVZHm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5c1035fb81492bb8.css","static/chunks/pages/index-dd67dcbc4fd7df18.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
